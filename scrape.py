a4e5eb6a99bd722420a9dc8f21658a7d520e3ebe
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2023-03-26 19:37:27 -0400

    Add 2022 teams list

--- scrape.py
+++ scrape.py
@@ -1,8 +1,39 @@
 #! python3
-import webbrowser, sys
+import webbrowser, sys, re
 
 if len(sys.argv) > 1:
-    address = ' '.join(sys.argv[1:])
-    print(address)
+    
+    teams = [
+        'vaughan',
+        'bluedevils',
+        'alliance',
+        'northtoronto',
+        'simcoe',
+        'prostars',
+        'guelph',
+        'sigma',
+        'hamilton',
+        'electriccity',
+        'woodbridge',
+        'scrosoppi',
+        'darby',
+        'pickering',
+        'waterloo',
+        'stcatherines',
+        'burlington',
+        'windsor',
+        'masters',
+        'unionville',
+        'northmississauga',
+        'london',
+        'ndc',
+        'tecumseh'
+    ]
+    team = re.sub('[^a-z]', '', sys.argv[1].lower())
+    
+    if team in teams:
+        print(team)
+    else:
+        print('no team found')
 else:
     print('no args?')
